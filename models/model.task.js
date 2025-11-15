1a1351bd8464a44671f393625b4ea6eefc8e0f3f
vais2004 <vkawale2004@gmail.com>
2025-11-15 21:37:46 +0530

    update feat: wrote the Schema in model.task.js and model.team.js

---
+++ models/model.task.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    team: { type: mongoose.Schema.Types.ObjectId, ref: "Team", required: true },
+    owners: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    ],
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    timeToComplete: { type: Number, required: true },
+    tags: [{ type: String }],
+    status: {
+      type: String,
+      enum: ["To Do", "In Progress", "Completed", "Blocked"],
+      default: "To Do",
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
---
+++ models/model.team.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: { type: String },
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+
+module.exports = mongoose.model("Team", teamSchema);
