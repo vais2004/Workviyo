ae200044616ddfc37da0ea03849c165619967ee7
vais2004 <vkawale2004@gmail.com>
2025-12-12 19:17:36 +0530

    update

--- models/model.task.js
+++ models/model.task.js
@@ -4,14 +4,15 @@ const taskSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     team: { type: mongoose.Schema.Types.ObjectId, ref: "Team", required: true },
-    owners: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    ],
     project: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Project",
       required: true,
     },
+    owners: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Member", required: true },
+    ],
+
     priority: {
       type: String,
       enum: ["Low", "Medium", "High"],
@@ -19,7 +20,6 @@ const taskSchema = new mongoose.Schema(
     },
     timeToComplete: { type: Number, required: true },
     tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
-
     status: {
       type: String,
       enum: ["To Do", "In Progress", "Completed", "Blocked"],
