922c0cde5349179c2607ea23d7ac232e7f73a33d
vais2004 <vkawale2004@gmail.com>
2025-12-16 15:28:51 +0530

    adding status in project schema

--- models/model.project.js
+++ models/model.project.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String },
+  status: {
+    type: String,
+    enum: ["To Do", "In Progress", "Completed", "Blocked"],
+    default: "To Do",
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
