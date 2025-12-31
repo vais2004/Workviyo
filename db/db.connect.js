9814a3722cb661530b105b9fb51371112cf87fbd
vais2004 <vkawale2004@gmail.com>
2025-12-31 16:46:08 +0530

    update

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,42 +1,42 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// const mongoUri = process.env.MONGODB;
-
-// const initializeDatabase = async () => {
-//   await mongoose
-//     .connect(mongoUri)
-//     .then(() => {
-//       console.log("Connected to Database");
-//     })
-//     .catch((error) => console.error("Error connecting to database", error));
-// };
-
-// module.exports = { initializeDatabase };
-
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-let isConnected = false;
+const mongoUri = process.env.MONGODB;
 
 const initializeDatabase = async () => {
-  if (isConnected) {
-    console.log("MongoDB already connected.");
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(process.env.MONGODB, {
-      serverSelectionTimeoutMS: 30000,
-      maxPoolSize: 10,
-    });
-
-    isConnected = db.connections[0].readyState === 1;
-
-    console.log("MongoDB Connected:", isConnected);
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-  }
+  await mongoose
+    .connect(mongoUri)
+    .then(() => {
+      console.log("Connected to Database");
+    })
+    .catch((error) => console.error("Error connecting to database", error));
 };
 
 module.exports = { initializeDatabase };
+
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// let isConnected = false;
+
+// const initializeDatabase = async () => {
+//   if (isConnected) {
+//     console.log("MongoDB already connected.");
+//     return;
+//   }
+
+//   try {
+//     const db = await mongoose.connect(process.env.MONGODB, {
+//       serverSelectionTimeoutMS: 30000,
+//       maxPoolSize: 10,
+//     });
+
+//     isConnected = db.connections[0].readyState === 1;
+
+//     console.log("MongoDB Connected:", isConnected);
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//   }
+// };
+
+// module.exports = { initializeDatabase };
